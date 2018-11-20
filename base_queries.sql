f8ba157611f7e5312db54a2db481e20d6c2cd91a
Stanislaw <7091832+asyndrige@users.noreply.github.com>
2018-11-20 15:55:32 +0300

    Update base_queries.sql

--- base_queries.sql
+++ base_queries.sql
@@ -1,15 +1,15 @@
 SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';
 
 SELECT 
-  c.table_name, c.column_name, c.ordinal_position, c.column_default, c.is_nullable, c.data_type, c.udt_name, 
-  c.character_maximum_length, c.character_octet_length, c.numeric_precision
+    c.table_name, c.column_name, c.ordinal_position, c.column_default, c.is_nullable, c.data_type, c.udt_name, 
+    c.character_maximum_length, c.character_octet_length, c.numeric_precision
 FROM 
-	information_schema.columns AS c 
+    information_schema.columns AS c 
 JOIN
-	information_schema.tables as t
+    information_schema.tables as t
 ON
-	t.table_name = c.table_name
+    t.table_name = c.table_name
 WHERE 
-  t.table_schema = 'public' AND t.table_type = 'BASE TABLE'
+    t.table_schema = 'public' AND t.table_type = 'BASE TABLE'
 ORDER BY 
-  c.table_name;
+    c.table_name;
